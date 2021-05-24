f34c09e1b75b25d3d8f163bb4de752a4455dfe46
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2021-05-24 21:33:47 +0100

    Displaying Data on the Home Page

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,16 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace Tasker.Pages
 {
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using System.Collections.Generic;
+    using System.Linq;
+    
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public IEnumerable<Task> Tasks { get; set; }
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Tasks = _context.Tasks.OrderBy(task => task.Priority);
         }
     }
 }
